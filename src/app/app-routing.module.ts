0d90e0b319c740589ea419eecefdb875a30aaa81
Guillermo Gonzalez <ggjnez92@gmail.com>
2020-01-15 19:16:52 +0100

    Add admin to the route of the application (#4)

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,31 +1,35 @@
-import { NgModule } from '@angular/core';
-import { CommonModule, } from '@angular/common';
-import { BrowserModule  } from '@angular/platform-browser';
-import { Routes, RouterModule } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {CommonModule,} from '@angular/common';
+import {BrowserModule} from '@angular/platform-browser';
+import {Routes, RouterModule} from '@angular/router';
 
-const routes: Routes =[
-  {
-    path: '',
-    redirectTo: 'dashboard',
-    pathMatch: 'full',
-  }, {
-    path: '',
-    children: [{
-      path: '',
-      loadChildren: './features/layouts/admin-layout/admin-layout.module#AdminLayoutModule'
-    }]
-  }
+const routes: Routes = [
+    {
+        path: '',
+        redirectTo: 'admin/dashboard',
+        pathMatch: 'full',
+    }, {
+        path: 'admin',
+        redirectTo: 'admin/dashboard',
+        pathMatch: 'full',
+    }, {
+        path: 'admin',
+        children: [{
+            path: '',
+            loadChildren: './features/layouts/admin-layout/admin-layout.module#AdminLayoutModule'
+        }]
+    }
 ];
 
 @NgModule({
-  imports: [
-    CommonModule,
-    BrowserModule,
-    RouterModule.forRoot(routes,{
-       useHash: true
-    })
-  ],
-  exports: [
-  ],
+    imports: [
+        CommonModule,
+        BrowserModule,
+        RouterModule.forRoot(routes, {
+            useHash: true
+        })
+    ],
+    exports: [],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
