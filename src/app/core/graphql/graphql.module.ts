dd1ae78fd8bc3b309517c83e15b5a3b3d3de2963
Guillermo Gonzalez <ggjnez92@gmail.com>
2020-01-17 00:45:33 +0100

    Create interceptor to inject jwt on each request (#3)

--- src/app/core/graphql/graphql.module.ts
+++ src/app/core/graphql/graphql.module.ts
@@ -1,11 +1,13 @@
 import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
+import {HTTP_INTERCEPTORS} from "@angular/common/http";
 
 import {ApolloModule, APOLLO_OPTIONS} from 'apollo-angular';
 import {HttpLinkModule, HttpLink} from 'apollo-angular-link-http';
 import {InMemoryCache} from 'apollo-cache-inmemory';
 
 import {environment} from '../../../environments/environment';
+import {AuthInterceptor} from "../interceptors/auth.interceptor";
 
 export function createApollo(httpLink: HttpLink) {
     return {
@@ -23,6 +25,11 @@ export function createApollo(httpLink: HttpLink) {
             useFactory: createApollo,
             deps: [HttpLink],
         },
+        {
+            provide: HTTP_INTERCEPTORS,
+            useClass: AuthInterceptor,
+            multi: true,
+        }
     ],
 })
 export class GraphQLModule {
