c54ec4f9f83f5190dd64bc3ca33c05311b77127b
Guillermo Gonzalez <ggjnez92@gmail.com>
2020-01-17 22:46:02 +0100

    Add edit and remove button to the list of posts (#12)

--- src/app/features/article/article.module.ts
+++ src/app/features/article/article.module.ts
@@ -4,12 +4,14 @@ import {CommonModule} from '@angular/common';
 import {OverviewComponent} from './overview/overview.component';
 import {ListComponent} from './list/list.component';
 import {ArticleRoutingModule} from './article-routing.module';
+import {MaterialModule} from "../../core/material/material.module";
 
 @NgModule({
     declarations: [OverviewComponent, ListComponent],
     imports: [
         CommonModule,
-        ArticleRoutingModule
+        ArticleRoutingModule,
+        MaterialModule
     ]
 })
 export class ArticleModule {
