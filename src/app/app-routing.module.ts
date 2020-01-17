1725b89374b4ad4f7943323f0344a32e774246b6
Guillermo Gonzalez <ggjnez92@gmail.com>
2020-01-17 22:11:22 +0100

    Remove hash from route (#15)

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -26,9 +26,7 @@ const routes: Routes = [
     imports: [
         CommonModule,
         BrowserModule,
-        RouterModule.forRoot(routes, {
-            useHash: true
-        })
+        RouterModule.forRoot(routes)
     ],
     exports: [],
 })
