a936d114df2d49aa7294f80dfdfec4f7ce6c43cb
Guillermo Gonzalez <ggjnez92@gmail.com>
2020-01-17 23:52:38 +0100

    Load post from store and update the interface (#12)

--- src/app/features/article/overview/overview.component.ts
+++ src/app/features/article/overview/overview.component.ts
@@ -1,28 +1,39 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {FormControl, FormGroup} from "@angular/forms";
+
+import {Store} from "@ngxs/store";
+
 import {Post} from "../../../core/redux/models";
-import {FormControl, FormGroup, Validators} from "@angular/forms";
+import {PostState} from "../../../core/redux/states";
 
 @Component({
-  selector: 'app-overview',
-  templateUrl: './overview.component.html',
-  styleUrls: ['./overview.component.scss']
+    selector: 'app-overview',
+    templateUrl: './overview.component.html',
+    styleUrls: ['./overview.component.scss']
 })
 export class OverviewComponent implements OnInit {
 
-  post: Post = {
-    body: ''
-  } as Post;
+    post: Post = {
+        body: ''
+    } as Post;
 
-  articleForm = new FormGroup({
-    body: new FormControl('')
-  });
+    articleForm = new FormGroup({
+        body: new FormControl('')
+    });
 
-  constructor() { }
+    constructor(private store: Store) {
+    }
 
-  ngOnInit() {
-  }
+    ngOnInit() {
+        this.store.select(PostState.post).subscribe(post => {
+            if (post) {
+                this.post = {...post};
+                this.articleForm.controls.body.setValue(this.post.body);
+            }
+        });
+    }
 
-  onBodyChange() {
-    this.post.body = this.articleForm.controls.body.value;
-  }
+    onBodyChange() {
+        this.post.body = this.articleForm.controls.body.value;
+    }
 }
