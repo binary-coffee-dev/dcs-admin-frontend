55b3c31c4a67313eacf22de068020cbfcf6b96e9
Guillermo Gonzalez <ggjnez92@gmail.com>
2020-01-17 15:13:12 +0100

    Create me query schema (#11)

--- src/app/core/graphql/mutations/login.ts
+++ src/app/core/graphql/mutations/login.ts
@@ -4,9 +4,6 @@ export const LOGIN_MUTATION = gql`
     mutation login($identifier: String!, $password: String!){
         login(input: {identifier: $identifier, password: $password}){
             jwt
-            user {
-                username
-            }
         }
     }
 `;
