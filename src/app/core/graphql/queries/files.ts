8ee96982eeb71cc10e7a0df5b64c25af95b3efb2
Guillermo Gonzalez <ggjnez92@gmail.com>
2020-01-20 21:35:44 +0100

    Sort request for get list of files (#17)

--- src/app/core/graphql/queries/files.ts
+++ src/app/core/graphql/queries/files.ts
@@ -2,7 +2,7 @@ import gql from 'graphql-tag';
 
 export const FILES_QUERY = gql`
     query pageQuery($limit: Int!, $start: Int!) {
-        uploadsConnection(limit: $limit, start: $start) {
+        uploadsConnection(sort: "createdAt:desc",limit: $limit, start: $start) {
             values {
                 name
                 url
